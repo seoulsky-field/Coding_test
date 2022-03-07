f72d5758f798bf7ab71fcc9e180e9fea0f45620e
전경민_T3187 <83350060+seoulsky-field@users.noreply.github.com>
2022-03-07 20:51:59 +0900

    부스트캠프 Medic팀 스터디 1주차 4번 - GCD 합 풀이 (22. 03. 07.)

---
+++ BaekJoon/BaekJoon3036.py
@@ -0,0 +1,12 @@
+import sys
+from fractions import Fraction    # 기약 분수로 나타내는 Python Library
+input = sys.stdin.readline
+
+N = int(input())    # 링의 개수를 입력
+rings = list(map(int, input().strip().split(' ')))    # 링의 반지름들을 입력
+
+for i in range(1, N):    # 첫 번째 링을 제외한 나머지 링들에 대해서 진행
+    if rings[0] % rings[i] == 0:    # 두 수가 정수꼴로 나눠지면
+        print(str(int(rings[0]/rings[i])) + '/' + '1')    # 분모가 1인 분수꼴로 표현
+    else:    # 두 수가 정수꼴로 나눠지지 않는다면
+        print(Fraction(rings[0], rings[i]))    # Fraction을 이용해 분수로 표현
