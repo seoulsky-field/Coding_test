c26966e8c88bd15daf16e24ddd835379f7f39e0e
전경민_T3187 <83350060+seoulsky-field@users.noreply.github.com>
2022-04-14 09:18:46 +0900

    부스트캠프 Medic팀 6주차 4번 - 여우는 어떻게 울지?(22. 04. 14.)

--- BaekJoon/BaekJoon9536.py
+++ BaekJoon/BaekJoon9536.py
@@ -12,6 +12,7 @@ for _ in range(T):    # 테스트 케이스 개수만큼 반복
 
     while sentence != 'what does the fox say?':    # 문장이 ending 문장이 아닐 때까지
         sound = sentence.split()[2]    # 문장의 3번째 단어(index 2)에는 동물 울음 소리가 적혀있으므로 추출
+        
         while sound in animal_sounds:    # 입력받았던 동물 울음 소리들 중 방금 울음 소리가 존재하지 않을 때까지 계속 제거
             animal_sounds.remove(sound)
         sentence = input().strip()    # 문장을 입력받고 줄바꿈 문자 제거
